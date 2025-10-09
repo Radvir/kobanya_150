165aa6ee6d04e89db11eece6b4555e228493b0b8
Radvir <kovacstihamer500@gmail.com>
2025-10-09 09:54:32 +0200

    delete action fix

--- app_kobanya150/admin.py
+++ app_kobanya150/admin.py
@@ -22,18 +22,22 @@ class kobanya150_AlkalomAdmin(admin.ModelAdmin):
 
         self.message_user(request, f"{count} felhasználó hozzáadva a 'kobanya150_jelentkezo' csoporthoz.")
 
-    add_jelentkezok_to_group.short_description = "Jelentkezők hozzáadása a kobanya150_jelentkezo csoporthoz"
+    add_jelentkezok_to_group.short_description = "➕ Jelentkezők hozzáadása a kobanya150_jelentkezo csoporthoz"
     
     @admin.action(description="❌ Delete selected users from this Alkalom (and database)")
     def delete_selected_users(self, request, queryset):
         filter_horizontal = ('jelentkezok',)
         group = Group.objects.get(name="kobanya150_jelentkezo")
         deleted_count = 0
-
+        
         for alkalom in queryset:
-            users_to_delete = alkalom.jelentkezok.filter(groups=group)
-            deleted_count += users_to_delete.count()
-            users_to_delete.delete()  # deletes User objects themselves
+            # All jelentkezők for this alkalom
+            for user in alkalom.jelentkezok.all():
+                user_groups = user.groups.all()
+                
+                if user_groups.count() == 1 and group in user_groups:
+                    user.delete()
+                    deleted_count += 1
 
         self.message_user(
             request,
